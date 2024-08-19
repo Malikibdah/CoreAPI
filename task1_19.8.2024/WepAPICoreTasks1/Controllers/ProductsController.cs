71dc50051edfbb4e33e5f85848e4c4c193f88200
Malikibdah <malikibdah88@gmail.com>
2024-08-20 00:37:12 +0300

    task1_19.8.2024

---
+++ task1_19.8.2024/WepAPICoreTasks1/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WepAPICoreTasks1.Models;
+
+namespace WepAPICoreTasks1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+
+        public ProductsController(MyDbContext db) 
+        {
+            _db = db; 
+        }
+        [HttpGet]
+        public IActionResult getAllProducts()
+        {
+            var products = _db.Products.Include(m=>m.Category).ToList();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _db.Products.Include(m=>m.Category).FirstOrDefault(m=>m.ProductId==id);
+            return Ok(product);
+        }
+    }
+}
---
+++ task1_19.8.2024/WepAPICoreTasks1/Models/MyDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WepAPICoreTasks1.Models;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-G7O24FF;Database=categoryAPI;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__categori__19093A2B7E77631A");
+
+            entity.ToTable("categories");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryImage).IsUnicode(false);
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6EDAC926493");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.ProductImage).IsUnicode(false);
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+
+            //entity.HasOne(d => d.Category).WithMany(p => p.Products)
+            //    .HasForeignKey(d => d.CategoryId)
+            //    .HasConstraintName("FK__Products__Catego__398D8EEE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
